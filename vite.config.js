8ced0de5261daa25076f1593daed26cdde9bcdd1
Incode <incode@pedagogico.CEUMA.EDU.BR>
2026-07-01 08:58:48 -0300

    Refactor: Move secondary pages and css to src directory

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        cadastro: resolve(__dirname, 'cadastro.html'),
-        escolha_plano: resolve(__dirname, 'escolha_plano.html'),
-        selecao_negocio: resolve(__dirname, 'selecao_negocio.html'),
-        demo_delivery_ui: resolve(__dirname, 'demo_delivery_ui.html'),
-        perfil_usuario: resolve(__dirname, 'perfil_usuario.html'),
+        cadastro: resolve(__dirname, 'src/pages/cadastro.html'),
+        escolha_plano: resolve(__dirname, 'src/pages/escolha_plano.html'),
+        selecao_negocio: resolve(__dirname, 'src/pages/selecao_negocio.html'),
+        demo_delivery_ui: resolve(__dirname, 'src/pages/demo_delivery_ui.html'),
+        perfil_usuario: resolve(__dirname, 'src/pages/perfil_usuario.html'),
       },
     },
   },
